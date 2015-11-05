0ad3ece7735ba893fc682c13f9cfd5bf9f65b8ed
Zac Colley <hi@zaccolley.com>
2015-11-05 10:25:22 +0000

    Made * a string

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var telegram = require('node-telegram-bot-api'),
     app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get(*, function(req, res) {
+app.get('*', function(req, res) {
   res.send('Bot online!');
 });
 
