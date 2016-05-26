6d431ff9ead5b470f08b3d907f1de9cbb88f8261
Zac Colley <hi@zaccolley.com>
2016-05-26 14:17:53 +0100

    Revert "New TTS"
    This reverts commit 1d5285e6b73b9ab3af4c43c3611a579b69e24124.

--- utter.js
+++ utter.js
@@ -1,25 +1,8 @@
 require('dotenv').load();
 
-var telegram = require('node-telegram-bot-api');
-var request  = require('request');
-var stripEmoji = require('emoji-strip');
-var voices = require('./voices');
-
-var headers = {
-  'Pragma': 'no-cache',
-  'Origin': 'https://acapela-box.com',
-  'Accept-Encoding': 'gzip, deflate',
-  'Accept-Language': 'en-GB,en-US;q=0.8,en;q=0.6',
-  'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36',
-  'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-  'Accept': 'application/json, text/javascript, */*; q=0.01',
-  'Cache-Control': 'no-cache',
-  'X-Requested-With': 'XMLHttpRequest',
-  'Connection': 'keep-alive',
-  'Referer': 'https://acapela-box.com/AcaBox/index.php',
-  'DNT': '1',
-  'Cookie': '__utmt=1; __utma=221195784.1138614722.1462405107.1462405107.1462405107.1; __utmb=221195784.4.10.1462405107; __utmc=221195784; __utmz=221195784.1462405107.1.1.utmcsr=acapela-box.com|utmccn=(referral)|utmcmd=referral|utmcct=/AcaBox/index.php; acabox=vom76tou4nca012k3g317d3k71'
-};
+var telegram = require('node-telegram-bot-api'),
+    request  = require('request'),
+    stripEmoji   = require('emoji-strip');
 
 var bot = new telegram(process.env.TELEGRAM_TOKEN, { polling: true });
 
@@ -29,81 +12,99 @@ function replaceAll(str, find, replace) {
   return str.replace(new RegExp(find, 'g'), replace);
 }
 
-function getRandomVoice(voices) {
-  return voices[Math.floor(Math.random() * voices.length)];
-}
-
 // matches /utter
 bot.onText(/\/(utter) (.+)/, function (message, match) {
   console.log('/utter');
 
   var chatId = message.chat.id;
-  var text = match[2].trim();
+  var text = match[2];
 
   console.log('Text sent to utter: ', text);
 
-  if (!text || text.length <= 0) {
-    return bot.sendMessage(chatId, '✋ You need to add some text...');
+  if (!text || text.trim().length <= 0) {
+    bot.sendMessage(chatId, '✋ You need to add some text...');
+    return false;
   }
 
   // send message that it's upping some audio
   bot.sendChatAction(chatId, 'upload_audio');
 
-  var voice = 'will22k',
-      speed = 180, // 120 - 240 | Default: 180
-      pitch = 100; // 85 - 115 | Default: 100
+  var voices = [
+    'usenglishfemale', 'usenglishfemale2', 'usenglishmale',
+    'usenglishmale2', 'ukenglishfemale', 'ukenglishfemale2',
+    'ukenglishmale', 'auenglishfemale', 'usspanishfemale',
+    'usspanishmale', 'chchinesefemale', 'hkchinesefemale',
+    'jpjapanesefemale', 'krkoreanfemale', 'caenglishfemale',
+    'huhungarianfemale', 'brportuguesefemale', 'eurportuguesefemale',
+    'eurportuguesemale', 'eurspanishfemale', 'eurspanishmale',
+    'eurcatalanfemale', 'eurczechfemale', 'eurdanishfemale',
+    'eurfinnishfemale', 'eurfrenchfemale', 'eurfrenchmale',
+    'eurnorwegianfemale', 'eurdutchfemale', 'eurpolishfemale',
+    'euritalianfemale', 'euritalianmale', 'eurturkishfemale',
+    'eurturkishmale', 'eurgreekfemale', 'eurgermanfemale',
+    'eurgermanmale', 'rurussianfemale', 'rurussianmale',
+    'swswedishfemale', 'cafrenchfemale', 'cafrenchmale'
+  ];
+
+  var voice = 'ukenglishmale',
+      speed = 0,
+      pitch = 0;
 
   // if there's a flag set the relevant voice
+
   if ( text.indexOf('🇺🇸') !== -1 ) {
-    voice = getRandomVoice(voices.filter(voice => voice.lang === 'en-US')).id;
+    voice = 'usenglishmale';
   } else if ( text.indexOf('🇯🇵') !== -1 || text.indexOf('🗾') !== -1 ) {
-    voice = getRandomVoice(voices.filter(voice => voice.lang === 'ja-JP')).id;
+    voice = 'jpjapanesefemale';
   } else if ( text.indexOf('🇨🇳') !== -1 ) {
-    voice = getRandomVoice(voices.filter(voice => voice.lang === 'zh-CN')).id;
+    voice = 'chchinesefemale';
   } else if ( text.indexOf('🇰🇷') !== -1 ) {
-    voice = getRandomVoice(voices.filter(voice => voice.lang === 'ko-KR')).id;
+    voice = 'krkoreanfemale';
   } else if ( text.indexOf('🇨🇦') !== -1 || text.indexOf('🍁') !== -1 ) {
-    voice = getRandomVoice(voices.filter(voice => voice.lang === 'fr-CA')).id;
+    voice = 'caenglishfemale';
+  } else if ( text.indexOf('🇭🇺') !== -1) {
+    voice = 'huhungarianfemale';
   } else if ( text.indexOf('🇧🇷') !== -1) {
-    voice = getRandomVoice(voices.filter(voice => voice.lang === 'pt-BR')).id;
+    voice = 'brportuguesefemale';
+  } else if ( text.indexOf('🇧🇷') !== -1) {
+    voice = 'brportuguesefemale';
   } else if ( text.indexOf('🇵🇹') !== -1) {
-    voice = getRandomVoice(voices.filter(voice => voice.lang === 'pt-PT')).id;
+    voice = 'eurportuguesefemale';
   } else if ( text.indexOf('🇪🇸') !== -1) {
-    voice = getRandomVoice(voices.filter(voice => voice.lang === 'es-ES')).id;
-  }
-
-  // if its the crown, pound note or guard then it's the queen voice
-  if (text.indexOf('👑') !== -1 || text.indexOf('💷') !== -1 || text.indexOf('💂') !== -1) {
-    voice = voices.filter(voice => voice.special === 'thequeen')[0].id;
-  }
-
-  // if we get the scary masks use the bad guy voice
-  if (text.indexOf('👺') !== -1 || text.indexOf('👹') !== -1) {
-    voice = voices.filter(voice => voice.special === 'badguy')[0].id;
-  }
-
-  // if we get the old man then use the old man voice
-  if (text.indexOf('👴') !== -1) {
-    voice = voices.filter(voice => voice.special === 'oldman')[0].id;
-  }
-
-  // if we get the dragons or frog then use the yoda voice
-  if (text.indexOf('🐲') !== -1 || text.indexOf('🐉') !== -1 || text.indexOf('🐸') !== -1) {
-    voice = voices.filter(voice => voice.special === 'yoda')[0].id;
+    voice = 'eurspanishfemale';
+  } else if ( text.indexOf('🇨🇿') !== -1) {
+    voice = 'eurczechfemale';
+  } else if ( text.indexOf('🇩🇰') !== -1) {
+    voice = 'eurdanishfemale';
+  } else if ( text.indexOf('🇫🇮') !== -1) {
+    voice = 'eurfinnishfemale';
+  } else if ( text.indexOf('🇫🇷') !== -1) {
+    voice = 'eurfrenchfemale';
+  } else if ( text.indexOf('🇳🇴') !== -1) {
+    voice = 'eurnorwegianfemale';
+  } else if ( text.indexOf('🇳🇱') !== -1) {
+    voice = 'eurdutchfemale';
+  } else if ( text.indexOf('🇵🇱') !== -1) {
+    voice = 'eurpolishfemale';
+  } else if ( text.indexOf('🇮🇹') !== -1) {
+    voice = 'euritalianmale';
+  } else if ( text.indexOf('🇹🇷') !== -1) {
+    voice = 'eurturkishfemale';
+  } else if ( text.indexOf('🇬🇷') !== -1) {
+    voice = 'eurgreekfemale';
+  } else if ( text.indexOf('🇩🇪') !== -1) {
+    voice = 'eurgermanmale';
+  } else if ( text.indexOf('🇷🇺') !== -1) {
+    voice = 'rurussianfemale';
+  } else if ( text.indexOf('🇸🇪') !== -1) {
+    voice = 'swswedishfemale';
   }
 
   // check for snail or rabbit emoji for speeds
-  if ( text.indexOf('🐇') !== -1 || text.indexOf('🐰') !== -1) {
-    speed = 260;
-  } else if ( text.indexOf('🐌') !== -1 ) {
-    speed = 100;
-  }
-
-  // check for arrows for pitches
-  if ( text.indexOf('⬆') !== -1 || text.indexOf('🔼') !== -1 || text.indexOf('⏫') !== -1 || text.indexOf('⤴️') !== -1) {
-    pitch = 115;
-  } else if ( text.indexOf('⬇') !== -1 || text.indexOf('🔽') !== -1 || text.indexOf('⏬') !== -1 || text.indexOf('⤵️') !== -1) {
-    pitch = 85;
+  if ( text.indexOf('🐇') !== -1 || text.indexOf('🐰') != -1) {
+    speed = 5;
+  } else if ( text.indexOf('🐌') != -1 ) {
+    speed = -10;
   }
 
   // add sloppy
@@ -113,43 +114,24 @@ bot.onText(/\/(utter) (.+)/, function (message, match) {
         return "sloppy " + word;
       }
 
-      // if no sloppy just return
       return word;
     }).join(" ");
   }
 
-  // woll smoth
   if ( text.indexOf('😮') !== -1 ) {
     // replace all vowels with o
     text = replaceAll(text, '[aeiou]+', 'o');
   }
 
-  // remove emojis
-  text = stripEmoji(text);
+  var apiKey = '34b06ef0ba220c09a817fe7924575123';
 
-  // uri encode
-  text = encodeURIComponent(text);
+  var url = 'https://api.ispeech.org/api/rest.mp3' +
+            '?apikey=' + apiKey +
+            '&action=convert' +
+            '&voice=' + voice +
+            '&speed=' + speed +
+            '&pitch=' + pitch +
+            '&text=' + stripEmoji(text);
 
-  console.log(speed);
-  console.log(text.indexOf('⬇') !== -1);
-  console.log(pitch);
-
-  var dataString = `text=%5Cvct%3D${pitch}%5C%20%5Cspd%3D${speed}%5C ${text}&voice=${voice}&listen=1&format=MP3&codecMP3=1&spd=${speed}&vct=${pitch}`;
-
-  var options = {
-    url: 'https://acapela-box.com/AcaBox/dovaas.php',
-    method: 'POST',
-    headers: headers,
-    body: dataString
-  };
-
-  request(options, (error, response, body) => {
-    if (!error && response.statusCode == 200) {
-      var result = JSON.parse(body);
-      var url = result.snd_url;
-      console.log(url);
-      bot.sendVoice(chatId, request(url));
-    }
-  });
-
+  bot.sendVoice(chatId, request(url));
 });
